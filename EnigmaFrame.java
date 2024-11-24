edb059b7b4e9f409d5b1b90baaa666a0b5bd0f60
Ruth Teshome <ruthteshome@Ruths-MacBook-Air-2.local>
2024-11-24 15:09:57 -0500

    setting up frame and gui classes

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,1 +1,22 @@
+public class EnigmaFrame extends EnigmaGUI{
 
+    public class JComboBox {
+
+    }
+
+    public class JTextField {
+
+    }
+
+    public class JTextArea {
+
+    }
+
+    public class JButton {
+
+    }
+
+    public class JLabel {
+
+    }
+}
--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,1 +1,6 @@
+public class EnigmaGUI{
 
+    public static void main(String args[]) {
+        
+    }
+}
