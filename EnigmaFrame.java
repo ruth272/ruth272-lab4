7fe97b7d4230f34e900a429256c77b7c595912ac
Ruth Teshome <ruthteshome@Ruths-MacBook-Air-3.local>
2024-12-01 18:23:26 -0500

    encrption is running but outputing wrong message and decrypt is not working at all

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -48,35 +48,39 @@ public class EnigmaFrame extends JFrame{
         dpanel.add(new JLabel("output"));
         dpanel.add(output);
 
-        //ConvertActionListener text = new ConvertActionListener();
+        ConvertActionListener e = new ConvertActionListener();
 
-        encrypt.addActionListener(text -> convertText(true));
-        decrypt.addActionListener(text -> convertText(false));
-
+        //encrypt.addActionListener(text -> convertText(true));
+        //decrypt.addActionListener(text -> convertText(false));
+        encrypt.addActionListener(e);
+        decrypt.addActionListener(e);
+        
         this.add(panel);
         this.setDefaultCloseOperation(EnigmaFrame.EXIT_ON_CLOSE);
         this.pack();
     }
 
-    private void convertText(boolean encrpyt) {
-        try{
-        String text = input.getText();
+    private class ConvertActionListener implements ActionListener{
+        public void actionPerformed(ActionEvent ee) {
+            try{
+                String text = input.getText();
 
-        int inner = (int) rotor1.getSelectedItem();
-        int middle = (int) rotor2.getSelectedItem();
-        int outer = (int) rotor3.getSelectedItem();
-        String result = null;
-            String start = startChar.getText();
-            Enigma en = new Enigma(inner, middle, outer, start);
-            if(encrpyt = true) {
-                result = en.encrypt(text);
+                int inner = (int) rotor1.getSelectedItem();
+                int middle = (int) rotor2.getSelectedItem();
+                int outer = (int) rotor3.getSelectedItem();
+                String result = null;
+                String start = startChar.getText();
+                Enigma en = new Enigma(inner, middle, outer, start);
+                if(ee.equals(encrypt)) {
+                    result = en.encrypt(text);
+                }
+                else if(ee.equals(decrypt)){
+                    result = en.decrypt(text);
+                }
+                output.setText(result);
+                }catch(Exception e){
+                output.setText("Starting character needs to be exactly 3.");
             }
-            else {
-                result = en.decrypt(text);
-            }
-            output.setText(result);
-        }catch(Exception e){
-            output.setText("Starting character needs to be exactly 3.");
         }
     }
 }
