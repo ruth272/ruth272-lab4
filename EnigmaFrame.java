e76251e8ca83bf446f3f44436c541d435187abef
Ruth Teshome <ruthteshome@Ruths-Air-3.lan>
2024-11-30 23:56:08 -0500

    added all required parts of the GUI:

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -10,7 +10,10 @@ public class EnigmaFrame extends JFrame{
     private JComboBox<Integer> rotor1;
     private JComboBox<Integer> rotor2;
     private JComboBox<Integer> rotor3;
+    private JTextField startChar;
     
+    private final Integer[] num = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     public EnigmaFrame() {
         super();
         
@@ -18,15 +21,27 @@ public class EnigmaFrame extends JFrame{
         decrypt = new JButton("Decrypt");
         input = new JTextArea();
         output = new JTextArea();
+        rotor1 = new JComboBox<Integer>(num);
+        rotor2 = new JComboBox<Integer>(num);
+        rotor3 = new JComboBox<Integer>(num);
+        startChar = new JTextField(10);
 
 
         JPanel panel = new JPanel(new FlowLayout());
         panel.add(new JLabel("Inner"));
+        panel.add(rotor1);
         panel.add(new JLabel("Middle"));
+        panel.add(rotor2);
         panel.add(new JLabel("Out"));
+        panel.add(rotor3);
         panel.add(new JLabel("Initial Positions"));
+        panel.add(startChar);
         panel.add(encrypt);
         panel.add(decrypt);
+        panel.add(new JLabel("Input"));
+        panel.add(input);
+        panel.add(new JLabel("output"));
+        panel.add(output);
 
         this.add(panel);
         this.setDefaultCloseOperation(EnigmaFrame.EXIT_ON_CLOSE);
